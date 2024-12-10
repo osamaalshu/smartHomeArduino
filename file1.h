9de4e3759087d336013ad4d4884bba90301550b7
Osama Al-Shuaili <102117576+osamaalshu@users.noreply.github.com>
2024-12-10 19:07:37 +0000

    Add files via upload

---
+++ file1.h
@@ -0,0 +1,500 @@
+#ifndef FILE1_H
+#define FILE1_H
+
+const char* file1Data = R"([
+    {
+        "Date": "2024-12-02 1:48:08AM - 2024-12-02 1:50:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.03,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 1:50:38AM - 2024-12-02 1:51:08AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.006,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 1:51:08AM - 2024-12-02 2:09:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.208,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 2:09:38AM - 2024-12-02 2:11:08AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.125,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 2:11:08AM - 2024-12-02 2:34:08AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.35,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 2:34:08AM - 2024-12-02 2:36:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": 0.042,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 2:36:38AM - 2024-12-02 2:44:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.233,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 2:44:38AM - 2024-12-02 2:59:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": 0.25,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 2:59:38AM - 2024-12-02 3:09:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.167,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 3:09:38AM - 2024-12-02 3:11:38AM",
+        "Time REM_hr_": 0.033,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 3:11:38AM - 2024-12-02 3:12:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.017,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 3:12:38AM - 2024-12-02 3:30:08AM",
+        "Time REM_hr_": 0.292,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 3:30:08AM - 2024-12-02 3:31:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.025,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 3:31:38AM - 2024-12-02 3:53:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.367,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 3:53:38AM - 2024-12-02 4:06:08AM",
+        "Time REM_hr_": 0.208,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:06:08AM - 2024-12-02 4:06:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.008,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:06:38AM - 2024-12-02 4:07:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.017,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:07:38AM - 2024-12-02 4:30:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.383,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:30:38AM - 2024-12-02 4:53:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": 0.383,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:53:38AM - 2024-12-02 4:54:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.017,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:54:38AM - 2024-12-02 4:55:08AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.008,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:55:08AM - 2024-12-02 5:07:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.208,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 5:07:38AM - 2024-12-02 5:14:08AM",
+        "Time REM_hr_": 0.108,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 5:14:08AM - 2024-12-02 6:08:08AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.9,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 6:08:08AM - 2024-12-02 6:11:08AM",
+        "Time REM_hr_": 0.05,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 6:11:08AM - 2024-12-02 6:32:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.358,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 6:32:38AM - 2024-12-02 6:33:38AM",
+        "Time REM_hr_": 0.017,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 6:33:38AM - 2024-12-02 6:35:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.033,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 6:35:38AM - 2024-12-02 6:57:38AM",
+        "Time REM_hr_": 0.367,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 6:57:38AM - 2024-12-02 8:08:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 1.183,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:08:38AM - 2024-12-02 8:09:08AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.008,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:09:08AM - 2024-12-02 8:40:08AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.517,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:40:08AM - 2024-12-02 8:42:08AM",
+        "Time REM_hr_": 0.033,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:42:08AM - 2024-12-02 8:43:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.025,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:43:38AM - 2024-12-02 8:52:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.15,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:52:38AM - 2024-12-02 8:53:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.017,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:53:38AM - 2024-12-02 9:54:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 1.017,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 9:54:38AM - 2024-12-02 9:55:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": 0.017,
+        "Time core_hr_": null,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 9:55:38AM - 2024-12-02 9:57:38AM",
+        "Time REM_hr_": null,
+        "Time asleep_hr_": null,
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.033,
+        "Time deep_hr_": null,
+        "Time in bed_hr_": null
+    },
+    {
+        "Date": "2024-12-02 1:48:08 AM - 2024-12-02 1:50:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.042
+    },
+    {
+        "Date": "2024-12-02 1:50:38 AM - 2024-12-02 1:51:08 AM",
+        "Time awake_hr_": 0.008,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 1:51:08 AM - 2024-12-02 2:09:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.308
+    },
+    {
+        "Date": "2024-12-02 2:09:38 AM - 2024-12-02 2:11:08 AM",
+        "Time awake_hr_": 0.025,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 2:11:08 AM - 2024-12-02 2:34:08 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.383
+    },
+    {
+        "Date": "2024-12-02 2:36:38 AM - 2024-12-02 2:44:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.133
+    },
+    {
+        "Date": "2024-12-02 2:59:38 AM - 2024-12-02 3:09:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.167
+    },
+    {
+        "Date": "2024-12-02 3:11:38 AM - 2024-12-02 3:12:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.017
+    },
+    {
+        "Date": "2024-12-02 3:30:08 AM - 2024-12-02 3:31:38 AM",
+        "Time awake_hr_": 0.025,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 3:31:38 AM - 2024-12-02 3:53:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.367
+    },
+    {
+        "Date": "2024-12-02 4:06:08 AM - 2024-12-02 4:06:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.008
+    },
+    {
+        "Date": "2024-12-02 4:06:38 AM - 2024-12-02 4:07:38 AM",
+        "Time awake_hr_": 0.017,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:07:38 AM - 2024-12-02 4:30:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.383
+    },
+    {
+        "Date": "2024-12-02 4:53:38 AM - 2024-12-02 4:54:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.017
+    },
+    {
+        "Date": "2024-12-02 4:54:38 AM - 2024-12-02 4:55:08 AM",
+        "Time awake_hr_": 0.008,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 4:55:08 AM - 2024-12-02 5:07:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.208
+    },
+    {
+        "Date": "2024-12-02 5:14:08 AM - 2024-12-02 6:08:08 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.9
+    },
+    {
+        "Date": "2024-12-02 6:11:08 AM - 2024-12-02 6:32:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.358
+    },
+    {
+        "Date": "2024-12-02 6:33:38 AM - 2024-12-02 6:35:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.033
+    },
+    {
+        "Date": "2024-12-02 6:57:38 AM - 2024-12-02 8:08:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 1.183
+    },
+    {
+        "Date": "2024-12-02 8:08:38 AM - 2024-12-02 8:09:08 AM",
+        "Time awake_hr_": 0.008,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:09:08 AM - 2024-12-02 8:40:08 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.517
+    },
+    {
+        "Date": "2024-12-02 8:42:08 AM - 2024-12-02 8:43:38 AM",
+        "Time awake_hr_": 0.025,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:43:38 AM - 2024-12-02 8:52:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.15
+    },
+    {
+        "Date": "2024-12-02 8:52:38 AM - 2024-12-02 8:53:38 AM",
+        "Time awake_hr_": 0.017,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 8:53:38 AM - 2024-12-02 9:54:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 1.017
+    },
+    {
+        "Date": "2024-12-02 9:54:38 AM - 2024-12-02 9:55:38 AM",
+        "Time awake_hr_": 0.017,
+        "Time core_hr_": null
+    },
+    {
+        "Date": "2024-12-02 9:55:38 AM - 2024-12-02 9:57:38 AM",
+        "Time awake_hr_": null,
+        "Time core_hr_": 0.033
+    }
+]
+
+)";
+
+#endif // FILE1_H
